f65dd3feede75d5c3c7ee688f0cb7a9557f35e0c
Michele Picozzi <michele.picozzi@hotmail.it>
2023-07-20 15:18:53 +0800

    Navbar mobile and welcome page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+
+import logo from "../../images/logo.png";
+
+const NabarItem = ({ title, classProps }: { title: string; classProps: any }) => {
+    return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
 
 export default function Navbar() {
-    return <div>Navbar</div>;
+    const [toggleMenu, setToggleMenu] = useState(false);
+    return (
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((title, index) => (
+                    <NabarItem key={title + index} title={title} classProps="text-white" />
+                ))}
+                <li className="bg-[#2952E3] py-2 px-7 mx-4 cursor-pointer rounded-full hover:bg-[#2546bd]">Login</li>
+            </ul>
+            <div className="flex relative">
+                {toggleMenu ? (
+                    <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+                ) : (
+                    <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
+                )}
+                {toggleMenu && (
+                    <ul className="blue-glassmorphism z-10 flex flex-col justify-start items-end rounded-md fixed top-0 right-0 p-3 w-[70%] h-screen shadow-2xl md:hidden list-none text-white animate-slide-in">
+                        <li className="text-xl w-full my-2 hover:cursor-pointer hover:text-gray-300">
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                        </li>
+                        {["Market", "Exchange", "Tutorials", "Wallets"].map((title, index) => (
+                            <NabarItem key={title + index} title={title} classProps="text-white my-2 text-lg" />
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </nav>
+    );
 }
--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import Loader from "./Loader";
 
 export default function Welcome() {
-    return <div>Welcome</div>;
+    return (
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-gradient text-3xl sm:text-5xl text-white py-1">
+                        Send Crypto <br /> across the world
+                    </h1>
+                    <p className="text-left mt-5 text-white font-light w-11/2 md:9/12 text-base">
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
