344ea1fcbbf1fca0f04a7563219d774a43cb4d3e
Michele Picozzi <michele.picozzi@hotmail.it>
2023-07-25 18:33:35 +0800

    services tabs done

--- client/src/utils/constants.tsx
+++ client/src/utils/constants.tsx
@@ -7,4 +7,4 @@ import abi from "./Transactions.json";
 
 export const contractABI = abi.abi;
 
-export const contractAddress = "0x7cad2085FD456bFfDcCDc0D4e86ae7492D2E4b06";
+export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
